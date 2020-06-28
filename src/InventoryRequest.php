3ee9d2e4f166c9d482a5943abd5dbf3b6525db7d
Nathan Oertel <nathan@digitalclouddesigns.com>
2020-06-28 00:59:11 +0000

    Added offer and group functions

--- src/InventoryRequest.php
+++ src/InventoryRequest.php
@@ -17,7 +17,116 @@
 namespace eBayAPI;
 
 class InventoryRequest extends AbstractRequest {
+    const CONDITION_ENUM = array(
+        1000 => 'NEW',
+        2750 => 'LIKE_NEW',
+        1500 => 'NEW_OTHER',
+        1750 => 'NEW_WITH_DEFECTS',
+        2000 => 'MANUFACTURER_REFURBISHED',
+        2500 => 'SELLER_REFURBISHED',
+        3000 => 'USED_EXCELLENT',
+        4000 => 'USED_VERY_GOOD',
+        5000 => 'USED_GOOD',
+        6000 => 'USED_ACCEPTABLE',
+        7000 => 'FOR_PARTS_OR_NOT_WORKING'
+    );
+
     public function getInventoryLocations($parameters = array()) {
         return parent::get('/sell/inventory/v1/location', $parameters);
+    }
+
+    public function getInventoryLocation($merchantLocationKey) {
+        return parent::get('/sell/inventory/v1/location/'.$merchantLocationKey);
+    }
+
+    public function createInventoryLocation($merchantLocationKey, $locationData) {
+        return parent::post('/sell/inventory/v1/location/'.$merchantLocationKey, $locationData);
+    }
+
+    public function updateInventoryLocation($merchantLocationKey, $locationData) {
+        return parent::post('/sell/inventory/v1/location/'.$merchantLocationKey.'/update_location_details', $locationData);
+    }
+
+    public function createOrReplaceInventoryItem($sku, $inventoryItem) {
+        return parent::put('/sell/inventory/v1/inventory_item/'.$sku, $inventoryItem);
+    }
+
+    public function getInventoryItem($sku) {
+        return parent::get('/sell/inventory/v1/inventory_item/'.$sku);
+    }
+
+    public function getInventoryItems($limit, $offset) {
+        return parent::get('/sell/inventory/v1/inventory_item', array(
+            'limit' => $limit,
+            'offset' => $offset
+        ));
+    }
+
+    public function deleteInventoryItem($sku) {
+        return parent::delete('/sell/inventory/v1/inventory_item/'.$sku);
+    }
+
+    public function bulkUpdatePriceQuantity($requests) {
+        return parent::post('/sell/inventory/v1/bulk_update_price_quantity', array(
+            'requests' => $requests
+        ));
+    }
+
+    public function createOrReplaceInventoryItemGroup($inventoryItemGroupKey, $inventoryItemGroup) {
+        return parent::put('/sell/inventory/v1/inventory_item_group/'.$inventoryItemGroupKey, $inventoryItemGroup);
+    }
+
+    public function getInventoryItemGroup($inventoryItemGroupKey) {
+        return parent::get('/sell/inventory/v1/inventory_item_group/'.$inventoryItemGroupKey);
+    }
+
+    public function deleteInventoryItemGroup($inventoryItemGroupKey) {
+        return parent::delete('/sell/inventory/v1/inventory_item_group/'.$inventoryItemGroupKey);
+    }
+
+    public function createOffer($offer) {
+        return parent::post('/sell/inventory/v1/offer', $offer);
+    }
+
+    public function updateOffer($offerId, $offer) {
+        return parent::put('/sell/inventory/v1/offer/'.$offerId, $offer);
+    }
+
+    public function getOffers($sku, $limit, $offset) {
+        return parent::get('/sell/inventory/v1/offer', array(
+            'sku' => $sku,
+            'limit' => $limit,
+            'offset' => $offset
+        ));
+    }
+
+    public function getOffer($offerId) {
+        return parent::get('/sell/inventory/v1/offer/'.$offerId);
+    }
+
+    public function publishOffer($offerId) {
+        return parent::post('/sell/inventory/v1/offer/'.$offerId.'/publish');
+    }
+
+    public function deleteOffer($offerId) {
+        return parent::delete('/sell/inventory/v1/offer/'.$offerId);
+    }
+
+    public function withdrawOffer($offerId) {
+        return parent::post('/sell/inventory/v1/offer/'.$offerId.'/withdraw');
+    }
+
+    public function bulkMigrateListing($listingIds) {
+        $request = array();
+
+        foreach($listingIds as $listingId) {
+            $request[] = array(
+                'listingId' => $listingId
+            );
+        }
+
+        return parent::post('/sell/inventory/v1/bulk_migrate_listing', array(
+            'requests' => $request
+        ));
     }
 }
