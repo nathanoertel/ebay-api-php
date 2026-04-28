1727a0e1919f95affb3eda17fe85632532c1aaa1
Nathan Oertel <nathan@invergehq.com>
2026-04-28 12:49:01 -0400

    feat: added metadata request

---
+++ src/inventory/MetadataRequest.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Copyright (c) 2019 Digital Cloud Designs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+namespace eBayAPI\inventory;
+
+class MetadataRequest extends AbstractRequest {
+    public function getItemConditionPolicies($marketplaceId, array $categoryIds) {
+        $parameters = [];
+
+        if (count($categoryIds)) {
+            $parameters['filter'] = 'categoryIds:{'.implode('|', $categoryIds).'}';
+        }
+        $result = parent::get("/sell/metadata/v1/marketplace/$marketplaceId/get_item_condition_policies", $parameters);
+
+        return $result['body'];
+    }
+}
